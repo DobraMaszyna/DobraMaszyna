ed480c16bdaaf1d8e7669083b195274b0ac03b5b
TypoJak <typojak@gmail.com>
2022-04-27 20:41:58 +0200

    testy

--- components/Navbar/Index.jsx
+++ components/Navbar/Index.jsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
+import { useState, useEffect } from "react";
+import Link from "next/link";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import SearchBar from './SearchBar';
-import NavIcon from './NavIcon';
-import SearchedProducts from './SearchedProducts/Index';
+import SearchBar from "./SearchBar";
+import NavIcon from "./NavIcon";
+import SearchedProducts from "./SearchedProducts/Index";
 
 const Navbar = () => {
   const [scrollY, setScrollY] = useState(0);
   const [defaultNavState, setDefaultNavState] = useState(true);
 
-  if (typeof window !== 'undefined') {
-    window.addEventListener('scroll', () => {
+  if (typeof window !== "undefined") {
+    window.addEventListener("scroll", () => {
       setScrollY(window.scrollY);
     });
   }
@@ -27,17 +27,17 @@ const Navbar = () => {
 
   return (
     <NavbarStyled defaultNavState={defaultNavState}>
-      <div className='NavContainer'>
+      <div className="NavContainer">
         <SearchBar />
 
         <ul>
-          <NavIcon imgSrc='https://img.icons8.com/dotty/80/000000/scales.png' />
-          <Link href='/cart'>
+          <NavIcon imgSrc="https://img.icons8.com/dotty/80/000000/scales.png" />
+          <Link href="/cart">
             <a>
-              <NavIcon imgSrc='https://img.icons8.com/dotty/80/000000/shopping-cart.png' />
+              <NavIcon imgSrc="https://img.icons8.com/dotty/80/000000/shopping-cart.png" />
             </a>
           </Link>
-          <NavIcon imgSrc='https://img.icons8.com/dotty/80/000000/like.png' />
+          <NavIcon imgSrc="https://img.icons8.com/dotty/80/000000/like.png" />
         </ul>
       </div>
       <SearchedProducts defaultNavState={defaultNavState} />
@@ -52,12 +52,18 @@ const NavbarStyled = styled.nav`
   width: 100%;
   max-width: 100vw;
 
+  @media (max-width: 768px) {
+    .search {
+      background-color: red;
+    }
+  }
+
   .NavContainer {
     display: flex;
     align-items: center;
     flex-direction: row;
     width: 100%;
-    height: ${(props) => (props.defaultNavState ? '75px' : '58px')};
+    height: ${(props) => (props.defaultNavState ? "75px" : "58px")};
     padding: 0 2% 0 20%;
     background-color: #fff;
     box-shadow: 0px 4px 41px rgba(0, 0, 0, 0.1);
